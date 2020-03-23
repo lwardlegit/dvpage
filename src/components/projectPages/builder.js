28cf05d1f82c7d713432fa08cdfd56a2de94dd32
luther <lwardle@unca.edu>
2020-03-22 21:52:16 -0700

    added ara

--- src/components/projectPages/builder.js
+++ src/components/projectPages/builder.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import Carousel from 'react-bootstrap/Carousel'
 export default class Builder extends Component {
     state={
         tags:['react','javaScript','Node js','CSS']
@@ -42,7 +42,45 @@ export default class Builder extends Component {
                 </div>
 
                 <div className='Carosel'>
-                <img alt='mtgbuilder abilities and features'></img>
+                <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=First slide&bg=373940"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="../../images/builder/builder_1.png"
+      alt="initial app view"
+      width='100px'
+      height='100px'
+    />
+
+    <Carousel.Caption>
+      <h3>Initial App View</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Third slide&bg=20232a"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
                 </div>
             </div>
         )
